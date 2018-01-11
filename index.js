0639ec60503488a1f6a5bc2224ed92f77b522a6d
Adam Brown <adam@novoda.com>
2018-01-11 17:33:35 +0000

    exporting all child plugins via top level module

---
+++ index.js
@@ -0,0 +1,12 @@
+require('app-module-path').addPath(__dirname);
+
+module.exports.arcadeScore = require('plugins/arcade-score') 
+module.exports.bitcoin = require('plugins/bitcoin') 
+module.exports.clock = require('plugins/clock') 
+module.exports.contributors = require('plugins/contributors') 
+module.exports.countdown = require('plugins/countdown') 
+module.exports.jenkinsWall = require('plugins/jenkins-wall') 
+module.exports.novodaBlog = require('plugins/novoda-blog') 
+module.exports.playStoreReview = require('plugins/play-store-review') 
+module.exports.sonar = require('plugins/sonar') 
+module.exports.sonos = require('plugins/sonos') 
