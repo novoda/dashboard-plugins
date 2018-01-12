97b1dfb4ac5accd51845eac30901b938a383db82
Adam Brown <adam@novoda.com>
2018-01-12 10:31:24 +0000

    ignoring preinstall scripts and building plugins dir

--- install-child-modules.js
+++ install-child-modules.js
@@ -5,17 +5,18 @@ var cp = require('child_process')
 var os = require('os')
 
 // get library path
-var lib = resolve(__dirname, 'node_modules/')
+var lib = resolve(__dirname, 'plugins/')
 
 fs.readdirSync(lib)
   .forEach(function (mod) {
+    console.log(mod)
     var modPath = join(lib, mod)
-// ensure path has package.json
-if (!fs.existsSync(join(modPath, 'package.json'))) return
+    // ensure path has package.json
+    if (!fs.existsSync(join(modPath, 'package.json'))) return
 
-// npm binary based on OS
-var npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm'
+    // npm binary based on OS
+    var npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm'
 
-// install folder
-cp.spawn(npmCmd, ['i'], { env: process.env, cwd: modPath, stdio: 'inherit' })
-})
+    // install folder
+    cp.spawn(npmCmd, ['i'], { env: process.env, cwd: modPath, stdio: 'inherit' })
+  })
