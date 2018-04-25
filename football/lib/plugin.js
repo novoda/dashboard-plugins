8a5915bdc2523318785110646c414a078bd70777
Eduardo Urso <eduardourso@gmail.com>
2018-04-25 17:54:14 +0200

    Remove not needed template fields

--- football/lib/plugin.js
+++ football/lib/plugin.js
@@ -10,15 +10,7 @@ const configuration = () => {
     return {
         "football-plugin": {
             name: 'football-plugin',
-            template: {
-                awayTeamName: plugin.createStringField('awayTeamName'),
-                homeTeamName: plugin.createStringField('homeTeamName'),
-                homeTeamFlag: plugin.createStringField('homeTeamFlag'),
-                awayTeamFlag: plugin.createStringField('awayTeamFlag'),
-                goalsHomeTeam: plugin.createStringField('goalsHomeTeam'),
-                goalsAwayTeam: plugin.createStringField('goalsAwayTeam'),
-                competition: plugin.createStringField('competition'),
-            }
+            template: { }
         }
     }
 }
