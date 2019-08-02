e0c9e28930c0edb255a93c44f2da08c94418f4e3
Alex Curran <alex@novoda.com>
2019-08-02 18:06:08 +0200

    Log out at least some good info

--- berlin-transport/lib/data-source.js
+++ berlin-transport/lib/data-source.js
@@ -1,16 +1,30 @@
 const Rail = require('national-rail-darwin')
+const createHafas = require('vbb-hafas')
+const moment = require('moment')
 
 const generateViewState = (configuration) => {
+    createHafas('my-awesome-program').departures('900000024101', { duration: 10 })
+        .then(results => {
+            return results.map(result => {
+                return {
+                    when: moment(result.when).format('h:mm a'),
+                    direction: result.direction,
+                    line: result.line.name
+                }
+            })
+        })
+        .then(console.log)
+        .catch(console.error)
     const rail = new Rail(configuration.darwinToken.value)
     const stationCode = configuration.stationCode.value
-    return Promise.all([fetchStation(rail, stationCode),fetchStationBoard(rail, stationCode)])
+    return Promise.all([fetchStation(rail, stationCode), fetchStationBoard(rail, stationCode)])
         .then(results => {
             const departures = results[1].trainServices.map(each => ({
                 destination: each.destination.name,
                 expected: each.etd,
                 time: each.std,
                 platform: each.platform || '-',
-                callingAt: each.subsequentCallingPoints.reduce((acc, curr, index, array) => (acc += ` ${curr.locationName}${index !== array.length - 1 ? ',': ''}`), "Calling at").toUpperCase()
+                callingAt: each.subsequentCallingPoints.reduce((acc, curr, index, array) => (acc += ` ${curr.locationName}${index !== array.length - 1 ? ',' : ''}`), "Calling at").toUpperCase()
             }))
             return {
                 stationName: results[0][0].name,
